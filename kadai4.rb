8f08feb6ee2cb7bef0784bff050c486c84fe2fc3
Replit user <>
2022-12-27 09:58:59 +0000

    kadai1234 と宿題kadai5

--- kadai4.rb
+++ kadai4.rb
@@ -1,16 +1,16 @@
-print "学生䛾人数を入力してください >"
+print '学生䛾人数を入力してください >'
 num = gets.chomp.to_i
 
 list_student = []
 
 (1..num).each do |i|
   puts "#{i}番目の学生の名前は何ですか?"
-  print "名前 > "
+  print '名前 > '
   name = gets.chomp
   list_student << name
 end
-puts "-------------名簿---------------"
+puts '-------------名簿---------------'
 list_student.each_with_index do |student, index|
   puts "+ #{index + 1}番目䛾学生䛿#{student}です"
 end
-puts "-------------------------------" 
+puts '-------------------------------'
