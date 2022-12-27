a69455d04fcd5613204dbfcf380f30e8fe40d59a
Replit user <>
2022-12-27 09:54:18 +0000

    kadai1234

--- kadai4.rb
+++ kadai4.rb
@@ -1,18 +1,18 @@
 print "学生䛾人数を入力してください > "
 number = Integer(gets) rescue nil
 names = []
-i = 0
-while i < number
+count = 0
+while count < number
   puts "#{i+1} 番目䛾学生䛾名前䛿何ですか？"
   print "名前 >"
   stuName = gets
   names.push(stuName)
-  i = i + 1
+  count = count + 1
 end
-i = 0
+count = 0
 puts "-------------名簿--------------"
-while i < names.length
+while count < names.length
   puts "#{i} 番目䛾学生は #{names[i]} です"
-  i = i + 1
+  count = count + 1
 end
 puts "-------------------------------"
